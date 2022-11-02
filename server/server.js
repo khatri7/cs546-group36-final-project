3106a461cdef113c07d18b597416aacd715e9f08
Abhishek Khatri <abhishekkhatri98@gmail.com>
2022-11-02 12:54:34 -0400

    fix(server): :bug: add cors allowed methods to allow non simple methods

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ app.use((req, res, next) => {
 		'Access-Control-Allow-Headers',
 		'Origin, X-Requested-With, Content-Type, Accept'
 	);
+	res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
 	next();
 });
 
