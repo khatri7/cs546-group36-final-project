5c09f6992d45158ed44088f1415632d01bcd1187
Giri Nithin <girinithinyogendra@gmail.com>
2022-11-08 11:59:16 -0500

    LoginForm components created

---
+++ client/src/components/Login/LoginForm.jsx
@@ -0,0 +1,73 @@
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+import React from 'react';
+
+// Creating schema
+const schema = Yup.object().shape({
+	email: Yup.string()
+		.required('Email is a required field')
+		.email('Invalid email format'),
+	password: Yup.string()
+		.required('Password is a required field')
+		.min(8, 'Password must be at least 8 characters'),
+});
+
+function LoginForm() {
+	return (
+		<>
+			{/* Wrapping form inside formik tag and passing our schema to validationSchema prop */}
+			<Formik
+				validationSchema={schema}
+				initialValues={{ email: '', password: '' }}
+				onSubmit={(values) => {
+					alert(JSON.stringify(values));
+				}}
+			>
+				{({
+					values,
+					errors,
+					touched,
+					handleChange,
+					handleBlur,
+					handleSubmit,
+				}) => (
+					<div className="login">
+						<div className="form">
+							<form noValidate onSubmit={handleSubmit}>
+								<span>Login</span>
+								<input
+									type="email"
+									name="email"
+									onChange={handleChange}
+									onBlur={handleBlur}
+									value={values.email}
+									placeholder="Enter email id / username"
+									className="form-control inp_text"
+									id="email"
+								/>
+								<p className="error">
+									{errors.email && touched.email && errors.email}
+								</p>
+								<input
+									type="password"
+									name="password"
+									onChange={handleChange}
+									onBlur={handleBlur}
+									value={values.password}
+									placeholder="Enter password"
+									className="form-control"
+								/>
+								<p className="error">
+									{errors.password && touched.password && errors.password}
+								</p>
+								<button type="submit">Login</button>
+							</form>
+						</div>
+					</div>
+				)}
+			</Formik>
+		</>
+	);
+}
+
+export default LoginForm;
