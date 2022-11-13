9e6ef02272f13eb7f0084be475e9e24d5ac2d59f
Abhishek Khatri <abhishekkhatri98@gmail.com>
2022-11-13 02:13:15 -0500

    chore

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Routes as RRDRoutes, Route } from 'react-router-dom';
 import Home from 'pages';
-import Login from 'pages/Login';
+import Login from 'pages/hj';
 import Signup from 'pages/Signup';
 
 function Routes() {
