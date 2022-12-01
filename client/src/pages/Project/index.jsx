9bfcdf1024fa8fca4ee760d4b916df84a759003c
Abhishek Khatri <abhishekkhatri98@gmail.com>
2022-12-01 02:02:40 -0500

    fix(client): typo in comment

--- client/src/pages/Project/index.jsx
+++ client/src/pages/Project/index.jsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom';
 
 function Project() {
 	const { projectId } = useParams();
-	// checkout pages/users/index.jsx to see how data is being loaded
+	// checkout pages/user/index.jsx to see how data is being loaded
 	return <div>{projectId}</div>;
 }
 
